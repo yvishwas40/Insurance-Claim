fcc10dfbe6e1c9910efecbbd751fcf0abcb74237
yvishwas40 <yvishwas40@gmail.com>
2025-08-02 11:43:41 +0530

    solved Port issue

--- main.py
+++ main.py
@@ -68,3 +68,7 @@ async def run_query(payload: QueryRequest):
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Insurance Query Retrieval API 🚀"}
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
